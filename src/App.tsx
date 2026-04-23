8d171b3e6a2ff98929884ca047b6465a69cce4a9
cursossolange <s.henriquez.castets@gmail.com>
2026-04-23 13:39:14 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,5 @@
-import { useState } from "react";
 import LandingPage from "./components/LandingPage";
-import ProductArea from "./components/ProductArea";
 
 export default function App() {
-  const [isPurchased, setIsPurchased] = useState(false);
-
-  return (
-    <div className="min-h-screen">
-      {isPurchased ? (
-        <ProductArea />
-      ) : (
-        <LandingPage onPurchase={() => setIsPurchased(true)} />
-      )}
-    </div>
-  );
+  return <LandingPage />;
 }
