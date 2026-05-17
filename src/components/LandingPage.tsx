59c93df723e5068e1aba6edbadd9c4beda912988
cursossolange <s.henriquez.castets@gmail.com>
2026-05-16 23:21:38 -0400

    Update LandingPage.tsx

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -522,7 +522,7 @@ Al escribir lo que observaste, empiezas a reconocer patrones autom√°ticos con m√
      </div>
     </section>
 
-    <footer className="py-6 border-t border-slate-100 text-center bg-white">
+    
 
 <footer className="py-6 border-t border-slate-100 text-center bg-white">
   <div className="container mx-auto px-6">
@@ -533,7 +533,6 @@ Al escribir lo que observaste, empiezas a reconocer patrones autom√°ticos con m√
   </div>
  
 </footer>
-</div>
 );
 }
 
