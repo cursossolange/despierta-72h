4c282339b992c9696fe3ddba3a6337829cfaaf90
cursossolange <s.henriquez.castets@gmail.com>
2026-05-17 12:16:27 -0400

    Update LandingPage.tsx

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -522,7 +522,7 @@ Al escribir lo que observaste, empiezas a reconocer patrones autom√°ticos con m√
     </a>
   </div>
 </div>
-
+</section>
 
 
 <footer className="py-6 border-t border-slate-100 text-center bg-white">
@@ -533,7 +533,7 @@ Al escribir lo que observaste, empiezas a reconocer patrones autom√°ticos con m√
       ¬© 2026 Despierta 72H
     </p>
   </div>
-  </div>
+ 
 </footer>
 </>
 );
